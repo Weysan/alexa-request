828166e4621525d20ab5e9e8103477a3f4354c67
Raphaël GONCALVES <contact@raphael-goncalves.fr>
2017-03-14 18:39:03 +0100

    Add test and handle session attributes from the incoming requests

--- AlexaIncomingRequest.php
+++ AlexaIncomingRequest.php
@@ -2,41 +2,85 @@
 namespace Weysan\Alexa;
 
 use Symfony\Component\HttpFoundation\Request;
+use Weysan\Alexa\Response\SessionAttributes;
 
 class AlexaIncomingRequest
 {
     protected $requestBody = array();
 
+    protected $sessionAttributes;
+
     public function __construct(Request $request)
     {
         $this->requestBody = json_decode($request->getContent(), true);
+        $this->sessionAttributes = new SessionAttributes();
+        $this->parseSessionAttributes();
     }
 
+    /**
+     * @return $this
+     */
+    protected function parseSessionAttributes()
+    {
+        if (isset($this->requestBody['session']['attributes'])) {
+            foreach ($this->requestBody['session']['attributes'] as $key => $value) {
+                $this->sessionAttributes->addAttribute($key, $value);
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
     public function getAppId()
     {
         return $this->requestBody['session']['application']['applicationId'];
     }
 
+    /**
+     * @return SessionAttributes
+     */
+    public function getSessionAttributes()
+    {
+        return $this->sessionAttributes;
+    }
+
+    /**
+     * @return string
+     */
     public function getSessionId()
     {
         return $this->requestBody['session']['sessionId'];
     }
 
+    /**
+     * @return string
+     */
     public function getUserId()
     {
         return $this->requestBody['session']['user']['userId'];
     }
 
+    /**
+     * @return string
+     */
     public function getVersion()
     {
         return $this->requestBody['version'];
     }
 
+    /**
+     * @return string
+     */
     public function getRequestType()
     {
         return $this->requestBody['request']['type'];
     }
 
+    /**
+     * @return string
+     */
     public function getRequestIntent()
     {
         return $this->requestBody['request']['intent']['name'];
--- Tests/AlexaIncomingRequestTest.php
+++ Tests/AlexaIncomingRequestTest.php
@@ -4,6 +4,7 @@ namespace Weysan\Alexa\Tests;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Weysan\Alexa\AlexaIncomingRequest;
+use Weysan\Alexa\Response\SessionAttributes;
 
 class AlexaIncomingRequestTest extends TestCase
 {
@@ -20,5 +21,25 @@ class AlexaIncomingRequestTest extends TestCase
         $this->assertEquals("IntentRequest", $parser->getRequestType());
         $this->assertEquals("fakeuserID-hjfd-55674654", $parser->getUserId());
         $this->assertEquals("SessionId.hjkjhj-6546546-lfhdsjh", $parser->getSessionId());
+    }
+
+    public function testIncomingParsingWithSessionAttributes()
+    {
+        $body = json_encode([
+            'session' => [
+                'attributes' => [
+                    'key1' => 'value1',
+                    'key2' => 'value2',
+                    'key3' => 'value3',
+                ]
+            ]
+        ]);
+        $request = new Request(array(), array(), array(), array(), array(), array(), $body);
+        $parser = new AlexaIncomingRequest($request);
+
+        $this->assertTrue($parser->getSessionAttributes() instanceof SessionAttributes);
+        $this->assertEquals('value1', $parser->getSessionAttributes()->getAttribute('key1'));
+        $this->assertEquals('value2', $parser->getSessionAttributes()->getAttribute('key2'));
+        $this->assertEquals('value3', $parser->getSessionAttributes()->getAttribute('key3'));
     }
 }
